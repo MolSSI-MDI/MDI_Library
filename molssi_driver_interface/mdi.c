dfd33d0d5a82811b3df8855f86a966ed99af2b7e
Taylor Barnes <tbarnes1@vt.edu>
2018-08-16 16:45:01 -0400

    Add base files for the MolSSI Driver Interface

---
+++ molssi_driver_interface/mdi.c
@@ -0,0 +1,287 @@
+/* ----------------------------------------------------------------------
+   MDI - MolSSI Driver Interface
+   https://molssi.org/, Molecular Sciences Software Institute
+   Taylor Barnes, tbarnes1@vt.edu
+-------------------------------------------------------------------------
+
+Contents:
+   MDI_Init: Initializes a socket and sets it to listen
+   MDI_Open: Opens a socket and requests a connection with a specified host
+   MDI_Accept_Connection: Accepts an incoming connection request
+   MDI_Send: Sends data through the socket
+   MDI_Recv: Receives data from the socket
+   MDI_Send_Command: Sends a string of length MDI_COMMAND_LENGTH over the
+      socket
+   MDI_Recv_Command: Receives a string of length MDI_COMMAND_LENGTH over the
+      socket
+*/
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+// length of an MDI command in characters
+const int MDI_COMMAND_LENGTH = 12;
+
+// MDI data types
+const int MDI_INT    = 0;
+const int MDI_DOUBLE = 1;
+const int MDI_CHAR   = 2;
+
+
+/* Initialize MDI */
+int MDI_Init(int* sockfd_ptr)
+{
+  int ret;
+  int sockfd;
+  struct sockaddr_in serv_addr;
+  int port;
+  int reuse_value = 1;
+
+  port = 8021;
+
+  // write the hostname to a file
+  system("hostname > ../hostname");
+
+  // create the socket
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    error("Could not create socket");
+  }
+
+  // create the socket address
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(port);
+
+  // enable reuse of the socket
+  ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse_value, sizeof(int));
+  if (ret < 0) {
+    error("Could not reuse socket");
+  }
+
+  // bind the socket
+  ret = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+  if (ret < 0) {
+    error("Could not bind socket");
+  }
+
+  // start listening (the second argument is the backlog size)
+  ret = listen(sockfd, 20);
+  if (ret < 0) {
+    error("Could not listen");
+  }
+
+  *sockfd_ptr = sockfd;
+
+  return ret;
+}
+
+
+int MDI_Open(int* sockfd_ptr, int* inet_ptr, int* port_ptr, const char* hostname_ptr)
+{
+   int sockfd, ai_err;
+   int ret;
+
+   if (*inet_ptr>0) { // create a TCP socket
+     
+     struct sockaddr_in driver_address;
+     int i;
+     int port;
+     struct hostent* host_ptr;
+     FILE* hostfile;
+     char buff[255];
+
+     port = 8021;
+
+     hostfile = fopen("../hostname","r");
+     fgets(buff, 255, (FILE*)hostfile);
+
+     int hlen = 10;
+     hlen = strlen(buff);
+
+     char serv_host[hlen];
+     for (i=0; i < hlen; i++) {
+       serv_host[i] = buff[i];
+     }
+     serv_host[hlen-1] = '\0';
+
+     printf("Driver hostname: %s\n",serv_host);
+
+     // get the address of the host
+     host_ptr = gethostbyname(serv_host);
+     if (host_ptr == NULL) {
+       error("Error in gethostbyname");
+     }
+     if (host_ptr->h_addrtype != AF_INET) {
+       error("Unkown address type");
+     }
+
+     bzero((char *) &driver_address, sizeof(driver_address));
+     driver_address.sin_family = AF_INET;
+     driver_address.sin_addr.s_addr = 
+       ((struct in_addr *)host_ptr->h_addr_list[0])->s_addr;
+     driver_address.sin_port = htons(port);
+
+     // create the socket
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) {
+       error("Could not create socket");
+     }
+     printf("Here is the socket: %i\n",sockfd);
+
+     // connect to the driver
+     ret = connect(sockfd, (const struct sockaddr *) &driver_address, sizeof(struct sockaddr_un));
+     if (ret < 0) {
+       error("Could not connect to the driver");
+     }
+     
+   }
+   else { // create a unix socket
+
+     struct sockaddr_un serv_addr;
+
+     // fills up details of the socket addres
+     memset(&serv_addr, 0, sizeof(serv_addr));
+     serv_addr.sun_family = AF_UNIX;
+     strcpy(serv_addr.sun_path, "/tmp/ipi_");
+     strcpy(serv_addr.sun_path+9, hostname_ptr);
+  
+     // create the socket
+     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+     // connect through the socket
+     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) { 
+       perror("Error opening UNIX socket: path unavailable, or already existing"); exit(-1);
+     }
+   }
+
+   *sockfd_ptr=sockfd;
+
+   return ret;
+}
+
+
+int MDI_Accept_Connection(int* sockfd_ptr, int* connection_ptr)
+{
+  int sockfd=*sockfd_ptr;
+  int connection;
+
+  //accept a connection
+  connection = accept(sockfd, NULL, NULL);
+  if (connection < 0) {
+    error("Could not accept connection");
+  }
+
+  *connection_ptr = connection;
+
+  return 0;
+}
+
+
+int MDI_Send_Command(const char* data_ptr, int* sockfd_ptr)
+{
+   int i;
+   int n;
+   int sockfd=*sockfd_ptr;
+   int len=MDI_COMMAND_LENGTH;
+   char buffer[MDI_COMMAND_LENGTH];
+   int str_end;
+
+   strcpy(buffer, data_ptr);
+
+   //Fortran has trouble with null characters, so convert them to whitespace
+   str_end = 0;
+   for (i=0; i<MDI_COMMAND_LENGTH; i++) {
+     if( buffer[i] == '\0' ) {
+       str_end = 1;
+     }
+     if( str_end == 1 ) {
+       buffer[i] = ' ';
+     }
+   }
+
+   n = write(sockfd,buffer,len);
+   if (n < 0) { perror("Error writing to socket: server has quit or connection broke"); exit(-1); }
+
+   return 0;
+}
+
+
+int MDI_Send(const char* data_ptr, int* len_ptr, int* type_ptr, int* sockfd_ptr)
+{
+   int n;
+   int len=*len_ptr;
+   int type=*type_ptr;
+   int sockfd=*sockfd_ptr;
+
+   // determine the byte size of the data type being sent
+   int datasize;
+   if (type == MDI_INT) {
+     datasize = sizeof(int);
+   }
+   else if (type == MDI_DOUBLE) {
+     datasize = sizeof(double);
+   }
+   else if (type == MDI_CHAR) {
+     datasize = sizeof(char);
+   }
+
+   n = write(sockfd,data_ptr,len*datasize);
+   if (n < 0) { perror("Error writing to socket: server has quit or connection broke"); exit(-1); }
+
+   return 0;
+}
+
+
+int MDI_Recv(char* data_ptr, int* len_ptr, int* type_ptr, int* sockfd_ptr)
+{
+   int n, nr;
+   int len=*len_ptr;
+   int type=*type_ptr;
+   int sockfd=*sockfd_ptr;
+
+   // determine the byte size of the data type being received
+   int datasize;
+   if (type == MDI_INT) {
+     datasize = sizeof(int);
+   }
+   else if (type == MDI_DOUBLE) {
+     datasize = sizeof(double);
+   }
+   else if (type == MDI_CHAR) {
+     datasize = sizeof(char);
+   }
+
+   n = nr = read(sockfd,data_ptr,len*datasize);
+
+   while (nr>0 && n<len*datasize )
+   {  nr=read(sockfd,&data_ptr[n],len-n); n+=nr; }
+
+   if (n == 0) { perror("Error reading from socket: server has quit or connection broke"); exit(-1); }
+
+   return 0;
+}
+
+
+int launch_server(const char* line)
+{
+  // create a fork
+  int pid = fork();
+
+  if (pid == 0) {
+    // child process
+    system(line);
+    exit(0);
+  }
+
+  return 0;
+}
+
---
+++ molssi_driver_interface/mdi_f90.f90
@@ -0,0 +1,262 @@
+! Fortran 90 wrapper for the MolSSI Driver Interface
+
+   MODULE F90MESSAGES
+   USE ISO_C_BINDING
+   
+   IMPLICIT NONE
+
+   INTEGER, PROTECTED, BIND(C, name="MDI_COMMAND_LENGTH") :: MDI_COMMAND_LENGTH
+
+   INTEGER, PROTECTED, BIND(C, name="MDI_INT")            :: MDI_INT
+   INTEGER, PROTECTED, BIND(C, name="MDI_DOUBLE")         :: MDI_DOUBLE
+   INTEGER, PROTECTED, BIND(C, name="MDI_CHAR")           :: MDI_CHAR
+
+  INTERFACE MDI_Send
+      MODULE PROCEDURE MDI_Send_s, &
+                       MDI_Send_d, MDI_Send_dv, &
+                       MDI_Send_i, MDI_Send_iv
+  END INTERFACE 
+
+  INTERFACE MDI_Recv
+      MODULE PROCEDURE MDI_Recv_s, &
+                       MDI_Recv_d, MDI_Recv_dv, &
+                       MDI_Recv_i, MDI_Recv_iv
+  END INTERFACE 
+
+  INTERFACE
+
+     FUNCTION MDI_Init_(sockfd_ptr) bind(c, name="MDI_Init")
+       USE, INTRINSIC :: iso_c_binding
+       INTEGER(KIND=C_INT)                      :: sockfd_ptr
+       INTEGER(KIND=C_INT)                      :: MDI_Init_
+     END FUNCTION MDI_Init_
+
+     FUNCTION MDI_Accept_Connection_(sockfd_ptr, connection_ptr) bind(c, name="MDI_Accept_Connection")
+       USE, INTRINSIC :: iso_c_binding
+       INTEGER(KIND=C_INT)                      :: sockfd_ptr
+       INTEGER(KIND=C_INT)                      :: connection_ptr
+       INTEGER(KIND=C_INT)                      :: MDI_Accept_Connection_
+     END FUNCTION MDI_Accept_Connection_
+
+     FUNCTION MDI_Send_Command_(data_ptr, sockfd_ptr) bind(c, name="MDI_Send_Command")
+       USE, INTRINSIC :: iso_c_binding
+       TYPE(C_PTR), VALUE                       :: data_ptr
+       INTEGER(KIND=C_INT)                      :: sockfd_ptr
+       INTEGER(KIND=C_INT)                      :: MDI_Send_Command_
+     END FUNCTION MDI_Send_Command_
+
+     SUBROUTINE launch_server(line) bind(c, name="launch_server")
+       USE, INTRINSIC :: iso_c_binding
+       CHARACTER(KIND=C_CHAR), DIMENSION(*)     :: line
+     END SUBROUTINE launch_server
+
+     FUNCTION MDI_Open_(sockfd_ptr, inet_ptr, port_ptr, hostname_ptr) BIND(C, name="MDI_Open")
+       USE ISO_C_BINDING
+       INTEGER(KIND=C_INT)                      :: sockfd_ptr, inet_ptr, port_ptr
+       CHARACTER(KIND=C_CHAR), DIMENSION(*)     :: hostname_ptr
+       INTEGER(KIND=C_INT)                      :: MDI_Open_
+     END FUNCTION MDI_Open_
+
+     FUNCTION MDI_Send_(data_ptr, len_ptr, type_ptr, sockfd_ptr) BIND(C, name="MDI_Send")
+       USE ISO_C_BINDING
+       INTEGER(KIND=C_INT)                      :: len_ptr, type_ptr, sockfd_ptr
+       TYPE(C_PTR), VALUE                       :: data_ptr
+       INTEGER(KIND=C_INT)                      :: MDI_Send_
+     END FUNCTION MDI_Send_
+
+     FUNCTION MDI_Recv_(data_ptr, len_ptr, type_ptr, sockfd_ptr) BIND(C, name="MDI_Recv")
+       USE ISO_C_BINDING
+       INTEGER(KIND=C_INT)                      :: len_ptr, type_ptr, sockfd_ptr
+       TYPE(C_PTR), VALUE                       :: data_ptr
+       INTEGER(KIND=C_INT)                      :: MDI_Recv_
+     END FUNCTION MDI_Recv_
+  END INTERFACE
+
+
+    
+  CONTAINS
+
+    SUBROUTINE MDI_Init(sockfd_ptr, ierr)
+      IMPLICIT NONE
+      INTEGER, INTENT(OUT) :: sockfd_ptr, ierr
+
+      ierr = MDI_Init_(sockfd_ptr)
+    END SUBROUTINE MDI_Init
+
+    SUBROUTINE MDI_Accept_Connection(sockfd_ptr, connection_ptr, ierr)
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: sockfd_ptr
+      INTEGER, INTENT(OUT) :: connection_ptr, ierr
+
+      ierr = MDI_Accept_Connection_(sockfd_ptr, connection_ptr)
+    END SUBROUTINE MDI_Accept_Connection
+
+    SUBROUTINE MDI_Open(sockfd_ptr, inet_ptr, port_ptr, hostname_ptr, ierr)
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: inet_ptr, port_ptr
+      INTEGER, INTENT(OUT) :: sockfd_ptr, ierr
+      CHARACTER(LEN=1024), INTENT(IN) :: hostname_ptr
+      CHARACTER(LEN=1,KIND=C_CHAR) :: chost(1024)
+
+      CALL fstr2cstr(hostname_ptr, chost)
+      ierr = MDI_Open_(sockfd_ptr, inet_ptr, port_ptr, chost)
+    END SUBROUTINE MDI_Open
+
+    SUBROUTINE fstr2cstr(fstr, cstr, plen)
+      IMPLICIT NONE
+      CHARACTER(LEN=*), INTENT(IN) :: fstr
+      CHARACTER(LEN=1,KIND=C_CHAR), INTENT(OUT) :: cstr(:)
+      INTEGER, INTENT(IN), OPTIONAL :: plen
+      
+      INTEGER i,n
+      IF (PRESENT(plen)) THEN
+         n = plen
+         DO i=1,n
+            cstr(i) = fstr(i:i)
+         ENDDO
+      ELSE
+         n = LEN_TRIM(fstr)
+         DO i=1,n
+            cstr(i) = fstr(i:i)
+         ENDDO
+         cstr(n+1) = C_NULL_CHAR
+      END IF
+    END SUBROUTINE fstr2cstr
+
+    SUBROUTINE MDI_Send_Command(fstring, sockfd_ptr, ierr)
+      USE ISO_C_BINDING
+      CHARACTER(LEN=*), INTENT(IN)             :: fstring
+      INTEGER, INTENT(IN)                      :: sockfd_ptr
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      INTEGER                                  :: i
+      CHARACTER(LEN=1, KIND=C_CHAR), TARGET    :: cstring(MDI_COMMAND_LENGTH)
+
+      DO i = 1, LEN(fstring)
+         cstring(i) = fstring(i:i)
+      ENDDO
+      
+      !pad the command with spaces
+      DO i = LEN(fstring) + 1, MDI_COMMAND_LENGTH
+         cstring(i) = " "
+      END DO
+
+      ierr = MDI_Send_Command_(c_loc(cstring(1)), sockfd_ptr)
+    END SUBROUTINE MDI_Send_Command
+
+    SUBROUTINE MDI_Send_d (fdata, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      REAL(KIND=8), INTENT(IN)                 :: fdata
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      REAL(KIND=C_DOUBLE), TARGET              :: cdata
+
+      cdata = fdata
+      ierr = MDI_Send_(c_loc(cdata), 1, type_ptr, sockfd_ptr)
+    END SUBROUTINE MDI_Send_d
+
+    SUBROUTINE MDI_Send_i (fdata, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      INTEGER, INTENT(IN)                      :: fdata
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      INTEGER(KIND=C_INT), TARGET              :: cdata
+
+      cdata = fdata
+      ierr = MDI_Send_(c_loc(cdata), 1, type_ptr, sockfd_ptr)
+    END SUBROUTINE MDI_Send_i
+
+    SUBROUTINE MDI_Send_s (fstring, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      CHARACTER(LEN=*), INTENT(IN)             :: fstring
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      INTEGER                                  :: i
+      CHARACTER(LEN=1, KIND=C_CHAR), TARGET    :: cstring(len_ptr)
+
+      DO i = 1,len_ptr
+         cstring(i) = fstring(i:i)
+      ENDDO
+      ierr = MDI_Send_(c_loc(cstring(1)), len_ptr, type_ptr, sockfd_ptr)
+    END SUBROUTINE MDI_Send_s
+
+    SUBROUTINE MDI_Send_dv(fdata, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING  
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      REAL(KIND=8), INTENT(IN), TARGET         :: fdata(len_ptr)
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      ierr = MDI_Send_(c_loc(fdata(1)), len_ptr, type_ptr, sockfd_ptr)
+    END SUBROUTINE MDI_Send_dv
+
+    SUBROUTINE MDI_Send_iv(fdata, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING  
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      INTEGER(KIND=C_INT), TARGET              :: fdata(len_ptr)
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      ierr = MDI_Send_(c_loc(fdata(1)), len_ptr, type_ptr, sockfd_ptr)
+    END SUBROUTINE MDI_Send_iv
+
+    SUBROUTINE MDI_Recv_d (fdata, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      REAL(KIND=8), INTENT(OUT)                :: fdata
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      REAL(KIND=C_DOUBLE), TARGET              :: cdata
+
+      ierr = MDI_Recv_(c_loc(cdata), 1, type_ptr, sockfd_ptr)
+      fdata=cdata
+    END SUBROUTINE MDI_Recv_d
+
+    SUBROUTINE MDI_Recv_iv (fdata, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      INTEGER(KIND=C_INT), INTENT(OUT), TARGET :: fdata(len_ptr)
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      ierr = MDI_Recv_(c_loc(fdata(1)), len_ptr, type_ptr, sockfd_ptr)
+    END SUBROUTINE MDI_Recv_iv
+
+    SUBROUTINE MDI_Recv_i (fdata, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      INTEGER, INTENT(OUT)                     :: fdata
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      INTEGER(KIND=C_INT), TARGET              :: cdata
+
+      ierr = MDI_Recv_(c_loc(cdata), 1, type_ptr, sockfd_ptr)
+      fdata = cdata
+    END SUBROUTINE MDI_Recv_i
+
+    SUBROUTINE MDI_Recv_s (fstring, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      CHARACTER(LEN=*), INTENT(OUT)            :: fstring
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      INTEGER                                  :: i
+      CHARACTER(LEN=1, KIND=C_CHAR), TARGET    :: cstring(len_ptr)
+
+      ierr = MDI_Recv_(c_loc(cstring(1)), len_ptr, type_ptr, sockfd_ptr)
+      fstring=""   
+      DO i = 1,len_ptr
+         fstring(i:i) = cstring(i)
+      ENDDO
+    END SUBROUTINE MDI_Recv_s
+
+    SUBROUTINE MDI_Recv_dv(fdata, len_ptr, type_ptr, sockfd_ptr, ierr)
+      USE ISO_C_BINDING  
+      INTEGER, INTENT(IN)                      :: len_ptr, type_ptr, sockfd_ptr
+      REAL(KIND=8), INTENT(OUT), TARGET        :: fdata(len_ptr)
+      INTEGER, INTENT(OUT)                     :: ierr
+
+      ierr = MDI_Recv_(c_loc(fdata(1)), len_ptr, type_ptr, sockfd_ptr)
+    END SUBROUTINE MDI_Recv_dv
+
+  END MODULE
