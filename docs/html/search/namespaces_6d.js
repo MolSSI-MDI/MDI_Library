43b2d22a193a2d836ebd7f6ff7341ab8430837fd
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-02-22 11:51:49 -0500

    Update documentation

--- docs/html/search/namespaces_6d.js
+++ docs/html/search/namespaces_6d.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['mdi_5fpython',['mdi_python',['../namespacemolssi__driver__interface_1_1mdi__python.html',1,'molssi_driver_interface']]]
+  ['mdi',['mdi',['../namespacemolssi__driver__interface_1_1mdi.html',1,'molssi_driver_interface']]]
 ];
