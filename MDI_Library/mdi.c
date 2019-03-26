ecc67a120cea2664be4564949a0d0f6a0d6588be
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-03-26 12:44:24 -0400

    Update .travis.yml

--- MDI_Library/mdi.c
+++ MDI_Library/mdi.c
@@ -22,16 +22,9 @@ Contents:
       socket
 */
 
-#include <signal.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/un.h>
-#include <unistd.h>
 #include <errno.h>
 #include "mdi.h"
 #include "communicator.h"
