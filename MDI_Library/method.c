8178c1555d21a129b78c0ec8b9c46bd5ba351835
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-03-26 12:58:28 -0400

    Update method.c includes

--- MDI_Library/method.c
+++ MDI_Library/method.c
@@ -3,8 +3,9 @@
  * \brief Class definition for top-level manager of MDI operations
  */
 #ifdef _WIN32
+//  #include <winsock2.h>
+  #include <ws2tcpip.h>
   #include <windows.h>
-  #include <winsock2.h>
 #else
   #include <netinet/in.h>
   #include <sys/socket.h>
