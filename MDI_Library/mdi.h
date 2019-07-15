46fdc60f82f91ca2e8bae7de70a3ee3de35f2acc
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-07-15 15:25:01 -0400

    Define DllExport

--- MDI_Library/mdi.h
+++ MDI_Library/mdi.h
@@ -23,72 +23,74 @@ Contents:
 extern "C" {
 #endif
 
+#define DllExport   __declspec( dllexport )
+
 // type of an MDI communicator handle
-__declspec(dllexport) typedef int MDI_Comm;
+typedef int MDI_Comm;
 
 // type of an MDI datatype handle
-__declspec(dllexport) typedef int MDI_Datatype;
+typedef int MDI_Datatype;
 
 // MDI version number
-__declspec(dllexport) extern const double MDI_VERSION;
+DllExport extern const double MDI_VERSION;
 
 // length of an MDI command in characters
-__declspec(dllexport) extern const int MDI_COMMAND_LENGTH;
+DllExport extern const int MDI_COMMAND_LENGTH;
 
 // length of an MDI name in characters
-__declspec(dllexport) extern const int MDI_NAME_LENGTH;
+DllExport extern const int MDI_NAME_LENGTH;
 
 // value of a null communicator
-__declspec(dllexport) extern const MDI_Comm MDI_NULL_COMM;
+DllExport extern const MDI_Comm MDI_NULL_COMM;
 
 // MDI data types
-__declspec(dllexport) extern const int MDI_INT;
-__declspec(dllexport) extern const int MDI_DOUBLE;
-__declspec(dllexport) extern const int MDI_CHAR;
-__declspec(dllexport) extern const int MDI_INT_NUMPY;
-__declspec(dllexport) extern const int MDI_DOUBLE_NUMPY;
+DllExport extern const int MDI_INT;
+DllExport extern const int MDI_DOUBLE;
+DllExport extern const int MDI_CHAR;
+DllExport extern const int MDI_INT_NUMPY;
+DllExport extern const int MDI_DOUBLE_NUMPY;
 
 // MDI communication types
-__declspec(dllexport) extern const int MDI_TCP;
-__declspec(dllexport) extern const int MDI_MPI;
-__declspec(dllexport) extern const int MDI_TEST;
+DllExport extern const int MDI_TCP;
+DllExport extern const int MDI_MPI;
+DllExport extern const int MDI_TEST;
 
 /*----------------------*/
 /* MDI unit conversions */
 /*----------------------*/
 
 // length
-__declspec(dllexport) extern const double MDI_METER_TO_BOHR;
-__declspec(dllexport) extern const double MDI_ANGSTROM_TO_BOHR;
+DllExport extern const double MDI_METER_TO_BOHR;
+DllExport extern const double MDI_ANGSTROM_TO_BOHR;
 
 // time
-__declspec(dllexport) extern const double MDI_SECOND_TO_AUT;
-__declspec(dllexport) extern const double MDI_PICOSECOND_TO_AUT;
+DllExport extern const double MDI_SECOND_TO_AUT;
+DllExport extern const double MDI_PICOSECOND_TO_AUT;
 
 // force
-__declspec(dllexport) extern const double MDI_NEWTON_TO_AUF;
+DllExport extern const double MDI_NEWTON_TO_AUF;
 
 // energy
-__declspec(dllexport) extern const double MDI_JOULE_TO_HARTREE;
-__declspec(dllexport) extern const double MDI_KJ_TO_HARTREE;
-__declspec(dllexport) extern const double MDI_KJPERMOL_TO_HARTREE;
-__declspec(dllexport) extern const double MDI_KCALPERMOL_TO_HARTREE;
-__declspec(dllexport) extern const double MDI_EV_TO_HARTREE;
-__declspec(dllexport) extern const double MDI_RYDBERG_TO_HARTREE;
-__declspec(dllexport) extern const double MDI_KELVIN_TO_HARTREE;
+DllExport extern const double MDI_JOULE_TO_HARTREE;
+DllExport extern const double MDI_KJ_TO_HARTREE;
+DllExport extern const double MDI_KJPERMOL_TO_HARTREE;
+DllExport extern const double MDI_KCALPERMOL_TO_HARTREE;
+DllExport extern const double MDI_EV_TO_HARTREE;
+DllExport extern const double MDI_RYDBERG_TO_HARTREE;
+DllExport extern const double MDI_KELVIN_TO_HARTREE;
 
-__declspec(dllexport) int MDI_Init(const char* options, void* world_comm);
-__declspec(dllexport) int MDI_Accept_Communicator();
-__declspec(dllexport) int MDI_Send(const void* buf, int count, MDI_Datatype datatype, MDI_Comm comm);
-__declspec(dllexport) int MDI_Recv(void* buf, int count, MDI_Datatype datatype, MDI_Comm comm);
-__declspec(dllexport) int MDI_Send_Command(const char* buf, MDI_Comm comm);
-__declspec(dllexport) int MDI_Recv_Command(char* buf, MDI_Comm comm);
-__declspec(dllexport) double MDI_Conversion_Factor(char* in_unit, char* out_unit);
+DllExport int MDI_Init(const char* options, void* world_comm);
+DllExport int MDI_Accept_Communicator();
+DllExport int MDI_Send(const void* buf, int count, MDI_Datatype datatype, MDI_Comm comm);
+DllExport int MDI_Recv(void* buf, int count, MDI_Datatype datatype, MDI_Comm comm);
+DllExport int MDI_Send_Command(const char* buf, MDI_Comm comm);
+DllExport int MDI_Recv_Command(char* buf, MDI_Comm comm);
+DllExport double MDI_Conversion_Factor(char* in_unit, char* out_unit);
 
 // only used internally by MDI
-__declspec(dllexport) void mdi_error(const char* message);
-__declspec(dllexport) int MDI_Get_MPI_Code_Rank();
-__declspec(dllexport) void MDI_Set_MPI_Intra_Rank(int rank);
+DllExport void mdi_error(const char* message);
+DllExport int MDI_Get_MPI_Code_Rank();
+DllExport void MDI_Set_MPI_Intra_Rank(int rank);
 
 #ifdef __cplusplus
 }
