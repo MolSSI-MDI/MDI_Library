fe677e09916156f4a229bd464761ace699e5545a
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-09-19 16:46:19 -0400

    Add ecosystem documentation section

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['mdi_20library',['MDI Library',['../library_page.html',1,'']]],
+  ['mdi_20ecosystem',['MDI Ecosystem',['../mdi_ecosystem.html',1,'']]],
   ['mdi_20standard',['MDI Standard',['../mdi_standard.html',1,'']]]
 ];
