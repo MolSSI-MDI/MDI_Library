f2fdc8fe4fb7a8a6f6cc8c77e8fff7e5c5309e97
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-09-26 13:39:06 -0400

    Update test scripts for Windows

--- tests/cxx-py_tcp.sh
+++ tests/cxx-py_tcp.sh
@@ -1,3 +1,3 @@
-./driver_cxx -mdi "-role DRIVER -name driver -method TCP -port 8021" &
+./$(find driver_cxx*) -mdi "-role DRIVER -name driver -method TCP -port 8021" &
 python engine_py.py -mdi "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost" &
 wait
--- tests/f90-f90_tcp.sh
+++ tests/f90-f90_tcp.sh
@@ -1,3 +1,3 @@
-./driver_f90 -mdi "-role DRIVER -name driver -method TCP -port 8021" &
-./engine_f90 -mdi "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost" &
+./$(find driver_f90*) -mdi "-role DRIVER -name driver -method TCP -port 8021" &
+./$(find engine_f90*) -mdi "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost" &
 wait
