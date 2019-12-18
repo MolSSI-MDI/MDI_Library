14c9289be296ee61c54e8f04a394ee6beaaa66c3
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-12-18 12:25:04 -0500

    Delete old test scripts

--- tests/f90-f90_tcp.sh
+++
@@ -1,3 +0,0 @@
-./$(find driver_f90*) -mdi "-role DRIVER -name driver -method TCP -port 8021" &
-./$(find engine_f90*) -mdi "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost" &
-wait
--- tests/py-f90_tcp.sh
+++
@@ -1,3 +0,0 @@
-python driver_py.py -mdi "-role DRIVER -name driver -method TCP -port 8021" &
-./$(find engine_f90*) -mdi "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost" &
-wait
