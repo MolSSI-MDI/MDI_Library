534cc41cc70491d0a1956f9c49860603a532ad15
taylor-a-barnes <taylor.a.barnes@gmail.com>
2021-03-11 13:30:36 -0500

    Update engine_cxx

--- tests/MDI_Test_Codes/engine_cxx/engine_cxx.h
+++ tests/MDI_Test_Codes/engine_cxx/engine_cxx.h
@@ -1,6 +1,8 @@
 #ifndef ENGINE_CXX
 #define ENGINE_CXX
 
+#include <mpi.h>
+
 // ensure that symbols are exported to Windows .dll files
 #ifdef _WIN32
   #define DllExport   __declspec( dllexport )
@@ -9,5 +11,8 @@
 #endif
 
 extern "C" DllExport int MDI_Plugin_init();
+int initialize_mdi(MDI_Comm* comm_ptr);
+int respond_to_commands(MDI_Comm comm, MPI_Comm mpi_world_comm);
+int execute_command(const char* command, MDI_Comm comm, void* class_obj);
 
 #endif
