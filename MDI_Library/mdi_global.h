74f5f7db03feb4fb7d7021fc3dc8803dc3e70c61
taylor-a-barnes <taylor.a.barnes@gmail.com>
2021-04-23 17:05:07 -0400

    Improve CMake package support

--- MDI_Library/mdi_global.h
+++ MDI_Library/mdi_global.h
@@ -34,7 +34,7 @@
 // MDI version numbers
 #define MDI_MAJOR_VERSION_ 1
 #define MDI_MINOR_VERSION_ 2
-#define MDI_PATCH_VERSION_ 6
+#define MDI_PATCH_VERSION_ 8
 
 // length of an MDI command in characters
 #define MDI_COMMAND_LENGTH_ 12
