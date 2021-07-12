050ffd6b41745c4889c2de81ebcd9169f4abfab3
taylor-a-barnes <taylor.a.barnes@gmail.com>
2021-07-12 09:31:23 -0400

    Update test_mdi.py

--- tests/test_mdi.py
+++ tests/test_mdi.py
@@ -66,7 +66,7 @@ def get_valgrind_options(valgrind):
                 "--trace-children=yes",
                 "--track-origins=yes",
                 "--error-exitcode=1",
-                "--gen-suppressions=yes",
+                "--gen-suppressions=all",
                 "--suppressions=" + os.path.dirname(os.path.realpath(__file__)) + "/valgrind.supp" ]
     else:
         return []
