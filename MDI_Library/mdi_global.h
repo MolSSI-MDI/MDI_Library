18b87a6bb69a6a4af0ea607a78119d9ee1bdefde
Taylor Barnes <taylor.a.barnes@gmail.com>
2022-04-14 19:58:09 +0000

    Update version number

--- MDI_Library/mdi_global.h
+++ MDI_Library/mdi_global.h
@@ -34,7 +34,7 @@
 // MDI version numbers
 #define MDI_MAJOR_VERSION_ 1
 #define MDI_MINOR_VERSION_ 3
-#define MDI_PATCH_VERSION_ 1
+#define MDI_PATCH_VERSION_ 2
 
 // length of an MDI command in characters
 #define MDI_COMMAND_LENGTH_ 12
