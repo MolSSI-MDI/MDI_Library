a737f58f250d2151df84a7a52a27b9bb892ac6d8
Jessica Nash <janash@vt.edu>
2022-06-22 19:38:44 +0000

    add test for element conversion

---
+++ tests/test_element.py
@@ -0,0 +1,36 @@
+import sys
+import pytest
+
+
+build_dir = "../build"
+
+sys.path.append(build_dir)
+
+def format_return(input_string):
+    my_string = input_string.decode('utf-8')
+
+    # remove any \r special characters, which sometimes are added on Windows
+    my_string = my_string.replace('\r','')
+
+    return my_string
+
+def import_mdi():
+    try: # Check for local build
+        sys.path.append('../build')
+        import MDI_Library as mdi
+    except ImportError: # Check for installed package
+        import mdi
+    return mdi
+
+
+##########################
+# Unit Conversions Tests #
+##########################
+
+
+def test_atomic_number():
+    mdi = import_mdi()
+
+    assert mdi.MDI_Atomic_Number("H") == 1
+    assert mdi.MDI_Atomic_Number("He") == 2
+    assert mdi.MDI_Atomic_Number("Li") == 3
